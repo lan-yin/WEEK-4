179697a9b2f2b35634719dcc0b671e0340376a3f
lan-yin <jay.braden1128@gmail.com>
2022-10-13 16:05:11 +0800

    week4Finish1st

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask  # 載入 Flask
-from flask import request  # 載入 Request
-from flask import redirect
-from flask import render_template
-from flask import session
+from flask import Flask, request, redirect, render_template, session, url_for
+
 
 app = Flask(__name__, static_folder="public", static_url_path="/")  
 app.secret_key = "noOneKnows"
@@ -13,22 +10,27 @@ def index():
 
 @app.route("/signin", methods = ["POST", "GET"])
 def signin():
-    account = request.form["account"]
+    user = request.form["user"]
     password = request.form["password"]
-    session["username"] = account
-    if account == "test" and password == "test":
+    if user == "test" and password == "test":
+        session["username"] = user
         return redirect("/member")  # 要的是網址，就要傳網址重新導向，不是傳檔案
     else:
         return redirect("/error?message=帳號、或密碼輸入錯誤")
 
 @app.route("/member")
 def member():
-    return render_template("member.html")
+    if "username" in session:
+        usernsme = session["username"]
+        if usernsme == "test":
+            return render_template("member.html")
+    else:
+        return redirect("/")
 
 @app.route("/error")
 def error():
-    mesg = request.args.get("message")
-    return render_template("error.html", mseg = "帳號、或密碼輸入錯誤")
+    mseg = request.args.get("message")
+    return render_template("error.html", message = mseg)
 
 
 @app.route("/signout")
@@ -36,5 +38,21 @@ def signout():
     session.pop("username", None)
     return redirect("/")
 
+
+@app.route("/input", methods = ["POST", "GET"])
+def input():
+    if request.method == "POST":
+        return redirect(url_for("square", number = request.form.get("number")))
+
+@app.route("/square/<number>")
+def square(number):
+    result = str(int(number) ** 2)
+    return render_template("square.html", result = result, number = number)
+
+
+
 # 啟動網站伺服器
 app.run(port=3000)
+
+
+
